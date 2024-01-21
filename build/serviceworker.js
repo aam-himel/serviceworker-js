e324c7d151ea902717b452c1fc7afc94f14ca90c
aam-himel <aam.himel@gmail.com>
2024-01-21 18:04:31 +0600

    modificaiton

--- build/serviceworker.js
+++ build/serviceworker.js
@@ -1,36 +1,5 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "addUserActivity") {
-        const newActivity = event.data.data;
-        addUserActivityToCollection(newActivity);
-    }
+self.addEventListener("message", (event) => { });
+self.addEventListener("install", () => {
+    console.log("installed!");
 });
-// Handle events when the tab is closed or the browser is closed
-self.addEventListener("beforeunload", (event) => __awaiter(void 0, void 0, void 0, function* () {
-    const storedData = localStorage.getItem("userActivityCollection");
-    let userActivityCollection;
-    if (storedData !== null) {
-        userActivityCollection = JSON.parse(storedData);
-    }
-    else {
-        userActivityCollection = [];
-    }
-    try {
-        const signedUrl = yield requestSignedUrl();
-        if (signedUrl) {
-            yield uploadToGoogleStorage(userActivityCollection, signedUrl);
-        }
-    }
-    catch (error) {
-        console.error("Error during upload:", error.message);
-    }
-}));
--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -1,28 +1,5 @@
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "addUserActivity") {
-    const newActivity = event.data.data;
-    addUserActivityToCollection(newActivity);
-  }
-});
+self.addEventListener("message", (event) => {});
 
-// Handle events when the tab is closed or the browser is closed
-self.addEventListener("beforeunload", async (event) => {
-  const storedData = localStorage.getItem("userActivityCollection");
-
-  let userActivityCollection: UserActivityCollection;
-
-  if (storedData !== null) {
-    userActivityCollection = JSON.parse(storedData);
-  } else {
-    userActivityCollection = [];
-  }
-
-  try {
-    const signedUrl = await requestSignedUrl();
-    if (signedUrl) {
-      await uploadToGoogleStorage(userActivityCollection, signedUrl);
-    }
-  } catch (error) {
-    console.error("Error during upload:", (error as Error).message);
-  }
+self.addEventListener("install", () => {
+  console.log("installed!");
 });
